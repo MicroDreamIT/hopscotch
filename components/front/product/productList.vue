265c7fd39d5aa7afdec9a821f65a1537b9008de8
Hasib <Hasib.arman.08@gmail.com>
2019-04-27 17:08:10 +0600

    product list

--- components/front/product/productList.vue
+++ components/front/product/productList.vue
@@ -1,7 +1,29 @@
 <template>
-    <div>
-        I am list
-    </div>
+    <mdb-container class="mt-5">
+        <mdb-row>
+            <mdb-card-group>
+                <mdb-card class="pr-1">
+                    <mdb-card-image src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%286%29.jpg"
+                                    alt="Card image cap"></mdb-card-image>
+                    <mdb-card-text>Cap Sleeves Top with Shorts and Headband Set in</mdb-card-text>
+                    <mdb-card-text>₹337 ₹529 (36% off)</mdb-card-text>
+                </mdb-card>
+                <mdb-card class="pr-1">
+                    <mdb-card-image src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%286%29.jpg"
+                                    alt="Card image cap"></mdb-card-image>
+                    <mdb-card-text>Cap Sleeves Top with Shorts and Headband Set in</mdb-card-text>
+                    <mdb-card-text>₹337 ₹529 (36% off)</mdb-card-text>
+                </mdb-card>
+                <mdb-card class="pr-1">
+                    <mdb-card-image src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%286%29.jpg"
+                                    alt="Card image cap"></mdb-card-image>
+                    <mdb-card-text>Cap Sleeves Top with Shorts and Headband Set in</mdb-card-text>
+                    <mdb-card-text>₹337 ₹529 (36% off)</mdb-card-text>
+                </mdb-card>
+            </mdb-card-group>
+
+        </mdb-row>
+    </mdb-container>
 </template>
 
 <script>
