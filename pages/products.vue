4711d699c61b23fb1bef36cfc208ffbfa6706453
MicroDreamIT <shahanur700@gmail.com>
2019-04-28 19:13:09 +0600

    product listing

--- pages/products.vue
+++ pages/products.vue
@@ -16,11 +16,9 @@
                                <mdb-btn color="danger" size="sm" rounded>Danger rounded </mdb-btn>
                            </div>
                             <div class="d-flex flex-wrap on-sm-device">
-                                <mdb-btn color="danger" size="sm" rounded>Danger rounded </mdb-btn>
-                                <mdb-btn color="danger" size="sm" rounded>Danger rounded </mdb-btn>
-                                <mdb-btn color="danger" size="sm" rounded>Danger rounded </mdb-btn>
-                                <mdb-btn color="danger" size="sm" rounded>Danger rounded </mdb-btn>
-                                <mdb-btn color="danger" size="sm" rounded>Danger rounded </mdb-btn>
+                                <mdb-btn color="pink" size="sm" rounded @click="triggerFilter(name)" v-for="name in ages" :key="name">
+                                    {{name}}
+                                </mdb-btn>
                             </div>
                             <!--<mdb-list-group horizontal>-->
                             <!--<mdb-list-group-item v-for="name in ages" :key="name" class="top-filter">-->
