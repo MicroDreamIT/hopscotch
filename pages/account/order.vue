cd4c69444396a384987be66214e6cfcf978588d4
MicroDreamIT <shahanur700@gmail.com>
2019-04-30 17:37:41 +0600

    account page

--- pages/account/order.vue
+++ pages/account/order.vue
@@ -18,22 +18,22 @@
 
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Orders</a>
+                                <a class="nav-link text-pink hover:text-pink-darker" href="#" >Orders</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Profile details</a>
+                                <a class="nav-link text-pink hover:text-pink-darker" href="#">Profile details</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">My Moments</a>
+                                <a class="nav-link text-pink hover:text-pink-darker" href="#">My Moments</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Saved Cards</a>
+                                <a class="nav-link text-pink hover:text-pink-darker" href="#">Saved Cards</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Credits</a>
+                                <a class="nav-link text-pink hover:text-pink-darker" href="#">Credits</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Sign Out</a>
+                                <a class="nav-link text-pink hover:text-pink-darker" href="#">Sign Out</a>
                             </li>
                         </ul>
                     </div>
@@ -68,6 +68,60 @@
                             </mdb-row>
                         </mdb-col>
                     </mdb-row>
+
+
+                    <mdb-row class="bg-white p-4 hover:shadow mb-4">
+                        <mdb-col md="6">
+                            <span>Arrives by May 7</span>
+                        </mdb-col>
+                        <mdb-col md="6">
+                            <mdb-row>
+                                <img class="block h-10 sm:h-12 mr-1"
+                                     src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                                <img class="block h-10 sm:h-12 mr-1"
+                                     src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                            </mdb-row>
+                        </mdb-col>
+                    </mdb-row>
+
+                    <mdb-row class="bg-white p-4 hover:shadow-lg">
+                        <mdb-col md="6">
+                            <span>Arrives by May 7</span>
+                        </mdb-col>
+                        <mdb-col md="6">
+                            <mdb-row>
+                                <img class="block h-10 sm:h-12 mr-1"
+                                     src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                                <img class="block h-10 sm:h-12 mr-1"
+                                     src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                            </mdb-row>
+                        </mdb-col>
+                    </mdb-row><mdb-row class="bg-white p-4 hover:shadow-lg">
+                    <mdb-col md="6">
+                        <span>Arrives by May 7</span>
+                    </mdb-col>
+                    <mdb-col md="6">
+                        <mdb-row>
+                            <img class="block h-10 sm:h-12 mr-1"
+                                 src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                            <img class="block h-10 sm:h-12 mr-1"
+                                 src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                        </mdb-row>
+                    </mdb-col>
+                </mdb-row><mdb-row class="bg-white p-4 hover:shadow">
+                    <mdb-col md="6">
+                        <span>Arrives by May 7</span>
+                    </mdb-col>
+                    <mdb-col md="6">
+                        <mdb-row>
+                            <img class="block h-10 sm:h-12 mr-1"
+                                 src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                            <img class="block h-10 sm:h-12 mr-1"
+                                 src="https://avatars1.githubusercontent.com/u/8063392?s=460&v=4" alt="">
+                        </mdb-row>
+                    </mdb-col>
+                </mdb-row>
+
 
                 </mdb-col>
                 <mdb-col md="2"></mdb-col>
