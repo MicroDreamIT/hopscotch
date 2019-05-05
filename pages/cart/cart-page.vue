3e94d3a8400ea2314b3d73862217a57286d6699e
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2019-05-05 17:04:55 +0600

    commit on cart  page

--- pages/cart/cart-page.vue
+++ pages/cart/cart-page.vue
@@ -1,103 +1,141 @@
 <template>
-    <div>
-      <mdb-container class="cart-details">
-          <mdb-row class="mt-lg-5 mb-lg-5">
-              <mdb-col md="2"></mdb-col>
-              <mdb-col md="5">
-                 <div class="cart-header">
-                        <span class="title">Cart</span>
-                        <span class="circle"> </span>
-                        <span class="count">{{cartItem.length}} items</span>
-
-                        <mdb-dropdown class="ow-dropdown ow-right">
-                            <mdb-dropdown-toggle slot="toggle">Enter pincode for delivery </mdb-dropdown-toggle>
-                            <mdb-dropdown-menu>
-                                <mdb-dropdown-item @click.prevent>
-                                    <label>Enter a pincode</label><br>
-                                    <input type="text"><br>
-                                    <mdb-btn  class="bg-primary" size="sm">Check</mdb-btn>
-
-                                </mdb-dropdown-item>
-
-                            </mdb-dropdown-menu>
-                        </mdb-dropdown>
+    <div style="background: #fff">
+        <div class="container">
+           <div class="row">
+             <div class="col-lg-8 lg-offset-2 mx-auto md-offset-0 xs-offset-0">
+                 <div class="row">
+                     <div class="col-lg-8 ">
+                         <br>
+                         <div class="cart-header">
+                             <span class="title">Cart</span>
+                             <span class="circle"> </span>
+                             <span class="count">{{cartItem.length}} items</span>
+                             <mdb-dropdown class="ow-dropdown ow-right adjust">
+                                 <mdb-dropdown-toggle slot="toggle">Enter pincode for delivery </mdb-dropdown-toggle>
+                                 <mdb-dropdown-menu>
+                                     <mdb-dropdown-item @click.prevent>
+                                         <label>Enter a pincode</label><br>
+                                         <input type="text"><br>
+                                         <mdb-btn  class="bg-primary" size="sm">Check</mdb-btn>
+                    
+                                     </mdb-dropdown-item>
+                
+                                 </mdb-dropdown-menu>
+                             </mdb-dropdown>
+                         </div>
+                         <hr>
+                         <div class="cart-body">
+                             <template v-for="(item,index) in cartItem">
+                                 <div class="inner">
+                                     <div>
+                                         <img :src="item.image" class="img-fluid d-block">
+                                         <select class="browser-default custom-select" v-model="item.quantity">
+                                             <option value="1">Qty 1</option>
+                                             <option value="2">Qty 2</option>
+                                             <option value="3">Qty 3</option>
+                                         </select>
+                    
+                                     </div>
+                                     <div>
+                                         <a href="">{{item.title}}</a> <span class="ow-right media-adjust"> <b>Arrives 10 May</b></span>
+                                         <p>by {{item.created_by}}</p>
+                                         <p class="ow-opacity-6">Size: {{item.size}}</p>
+                                         <p>₹ {{(parseFloat(item.orginal_price)*parseInt(item.quantity))-((parseInt(item.quantity)*
+                                             parseFloat(item.orginal_price))*parseFloat(item.discount))/100}}
+                                             <strike>₹ {{parseInt(item.quantity)*parseFloat(item.orginal_price)}}</strike>
+                                             <span class="text-success">{{item.discount}}% off</span>
+                            
+                                             <a href="" class="ow-right media-adjust" @click.prevent="deleteItem(index)">
+                                                 <mdb-icon far icon="trash-alt" class="icon-bg" />
+                                             </a>
+                                         </p>
+                    
+                                     </div>
+                                 </div>
+                                 <hr>
+                             </template>
+        
+        
+        
+        
+                         </div>
+                         <div class="cart-bottom mt-lg-5">
+                             <div>
+                                 <mdb-icon icon="shipping-fast" class="f25 d-flex justify-content-center flex-column pb-4 pt-4 fas fa-shipping-fast"/>
+                                 <h6>Free Shipping*</h6>
+                                 <span> On orders of ₹500 and above.Details</span>
+                             </div>
+                             <div>
+                                 <mdb-icon icon="reply-all" class="f25 d-flex justify-content-center flex-column pb-4 pt-4 fas fa-shipping-fast"/>
+                                 <h6>Easy returns</h6>
+                                 <span>Send items back for free within 15 days</span>
+                             </div>
+                             <div>
+                                 <mdb-icon icon="shield-alt" class="f25 d-flex justify-content-center flex-column pb-4 pt-4 fas fa-shipping-fast"/>
+                                 <h6>Secure shopping</h6>
+                                 <span>Your payment details are fully encrypted</span>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-lg-4">
+                         <br>
+                         <br>
+                         <div class="price_summary">
+                             <h5> <b>Price Summary</b></h5>
+                             <span class="includes">Includes GST and all government taxes</span>
+                             <div class="payment_term">
+                                 <p>Total item Price <span class="ow-right">$ {{totalItemPrice[0]}}</span></p>
+                                 <p>Item Discount <span class="ow-right">$ {{totalItemPrice[1]}}</span></p>
+                                 <p>Shipping fee <span class="ow-right">Free</span></p>
+                                 <div class="total"> Total <span class="ow-right">$ {{totalItemPrice[0]-totalItemPrice[1]}}</span></div>
+                             </div>
+                             <div class="proceed-button">
+                                 <mdb-btn color="pink" class="mt-lg-5 " block @click="proceedItem">Proceed to checkout</mdb-btn>
+                             </div>
+                           
+                         </div>
+                     </div>
                  </div>
-                  <hr>
-                 <div class="cart-body">
-                    <template v-for="(item,index) in cartItem">
-                        <div class="row">
-
-                                <mdb-col md="2">
-                                <img :src="item.image">
-                                <select class="browser-default custom-select" v-model="item.quantity">
-                                    <option value="1">Qty 1</option>
-                                    <option value="2">Qty 2</option>
-                                    <option value="3">Qty 3</option>
-                                </select>
-
-                            </mdb-col>
-                                <mdb-col md="10">
-                                    <a href="">{{item.title}}</a> <span class="ow-right">Arrives 10 May</span>
-                                    <p>by {{item.created_by}}</p>
-                                    <p class="ow-opacity-6">Size: {{item.size}}</p>
-                                    <p>₹ {{(parseFloat(item.orginal_price)*parseInt(item.quantity))-((parseInt(item.quantity)*
-                                        parseFloat(item.orginal_price))*parseFloat(item.discount))/100}}
-                                        <strike>₹ {{parseInt(item.quantity)*parseFloat(item.orginal_price)}}</strike>
-                                        <span class="text-success">{{item.discount}}% off</span>
-
-                                        <a href="" class="ow-right" @click.prevent="deleteItem(index)">
-                                            <mdb-icon far icon="trash-alt" /></a>
-                                    </p>
-
-                                </mdb-col>
-
-
-                        </div>
-                        <hr>
-                    </template>
-
-
-
-
-                </div>
-
-                  <div class="cart-bottom mt-lg-5">
-                      <div>
-                          <mdb-icon icon="shipping-fast" class="f25 d-flex justify-content-center pb-4 pt-4" />
-                          <h6>Free Shipping*</h6>
-                          <span> On orders of ₹500 and above.Details</span>
-                      </div>
-                      <div>
-                          <mdb-icon  icon="reply-all"  class="f25 d-flex justify-content-center pb-4 pt-4"/>
-                          <h6>Easy returns</h6>
-                          <span>Send items back for free within 15 days</span>
-                      </div>
-                      <div>
-                          <mdb-icon  icon="shield-alt" class="f25 d-flex justify-content-center pb-4 pt-4" />
-                          <h6>Secure shopping</h6>
-                          <span>Your payment details are fully encrypted</span>
-                      </div>
-                  </div>
-
-
-              </mdb-col>
-              <mdb-col md="3" class="cart-side-item">
-                  <h4> Price Summary</h4>
-                  <span class="includes">Includes GST and all government taxes</span>
-                      <div >
-                          <p>Total item Price <span class="ow-right">$ {{totalItemPrice[0]}}</span></p>
-
-                          <p>Item Discount <span class="ow-right">$ {{totalItemPrice[1]}}</span></p>
-                          <p>Shipping fee <span class="ow-right">Free</span></p>
-                          <hr>
-                          <h5> Total <span class="ow-right">$ {{totalItemPrice[0]-totalItemPrice[1]}}</span></h5>
-                      </div>
-                      <mdb-btn color="primary" class="mt-lg-5" block @click="proceedItem">Proceed to checkout</mdb-btn>
-
-              </mdb-col>
-              <mdb-col md="2"></mdb-col>
-          </mdb-row>
-      </mdb-container>
+             </div>
+           </div>
+        </div>
+        <div class="bottom-cart">
+            <!--<div>-->
+                <!--<a href="#total-cart">-->
+                    <!--<b>Total 985 <mdb-icon  icon="caret-down" /> </b><br>-->
+                    <!--<span>View price summery</span>-->
+                <!--</a>-->
+            <!--</div>-->
+            <div>
+                <!--<mdb-btn color="pink" class="mt-lg-5" block @click="proceedItem">Proceed to checkout</mdb-btn>-->
+            </div>
+        </div>
+        
+        <!--<div id="total-cart" class="total-cart">-->
+         <!---->
+                <!--<div class="checkout-head">-->
+                    <!--<span class="title">Cart</span>-->
+                    <!--<span class="count"> * {{cartItem.length}} items</span>-->
+                     <!--<span>  *  Total  ${{totalItemPrice[0]-totalItemPrice[1]}}</span>-->
+                     <!--<a href="#" class="ow-right pt-2"> <mdb-icon  icon="times" /> </a>-->
+                <!--</div>-->
+            <!--<br>-->
+            <!--<br>-->
+            <!--<br>-->
+            <!--<div class="price_summary">-->
+                <!--<h5> <b>Price Summary</b></h5>-->
+                <!--<span class="includes">Includes GST and all government taxes</span>-->
+                <!--<div class="payment_term">-->
+                    <!--<p>Total item Price <span class="ow-right">$ {{totalItemPrice[0]}}</span></p>-->
+                    <!--<p>Item Discount <span class="ow-right">$ {{totalItemPrice[1]}}</span></p>-->
+                    <!--<p>Shipping fee <span class="ow-right">Free</span></p>-->
+                    <!--<div class="total">Total <span class="ow-right">$ {{totalItemPrice[0]-totalItemPrice[1]}}</span></div>-->
+                <!--</div>-->
+                <!--<br>-->
+                <!--<mdb-btn color="pink" class="mt-lg-5" block @click="proceedItem">Proceed to checkout</mdb-btn>-->
+            <!--</div>-->
+        <!--</div>-->
+ 
     
     </div>
 </template>
@@ -152,12 +190,102 @@
     }
 </script>
 <style lang="scss" scoped="">
+    
+    
+    .price_summary{
+        @media(max-width: 768px){
+           // padding: 0 17px;
+        }
+    }
+    .checkout-head{
+        box-shadow: -1px -5px 11px #00000045;
+        position: absolute;
+        top: 0;
+        background: aliceblue;
+        left: 0;
+        right: 0;
+        padding: 15px;
+        color: #000;
+        span{
+            &:first-child{
+                font-size:25px;
+                color:#3c3c3c;
+            }
+            &:not(:first-child){
+                color:#797979;
+                font-size: 13px;
+            }
+        }
+    }
+    .total-cart{
+        display: none;
+        padding: 53px;
+        position: fixed;
+        bottom: -100%;
+        left: 0;
+        right: 0;
+        z-index: 112;
+        background: #fff;
+       &:target{
+           display: block;
+           bottom: 0;
+           top: 0;
+           transition: bottom 0.6s ease;
+           .price_summary{
+               display: block!important;
+           }
+       }
+    }
+    .media-adjust{
+        @media(max-width: 425px){
+            width: 100%;
+        }
+    }
+    .proceed-button{
+       @media(max-width: 768px){
+           box-shadow: 0 -6px 9px 0 rgba(0, 0, 0, 0.03);
+           display: block;
+           padding: 20px;
+           background-color: #fff;
+           position: fixed;
+           bottom: 0;
+           left: 0;
+           right: 0;
+           z-index: 12;
+           justify-content: space-around;
+           flex-wrap: wrap;
+       }
+    }
+
+ .icon-bg{
+     padding: 10px;
+     background: #EDEEF1;
+     border-radius: 3px;
+ }
+    .total{
+        padding:9px 0 ;
+        border-top: 1px dashed lightgrey;
+        font-weight: bold;
+    }
+    
+    .includes{
+        font-size: 12px;
+        color: #9a9a9a;
+    }
+    .payment_term{
+        margin-top:20px;
+        p{
+            line-height: 10px;
+            font-size: 13px;
+            color: #232222;
+        }
+    }
 
     .cart-bottom{
         display: flex;
         div{
             color: rgba(0,0,0,.56);
-            background-color: rgba(221, 222, 224, 0.72);
+            background-color: #F9FAFB;
             padding: 20px;
             margin-right: 15px;
             @media (max-width: 425px) {
@@ -186,10 +314,7 @@
     }
     .cart-side-item{
         padding:0 60px;
-        .includes{
-            opacity: .3;
-            font-size: 12px;
-        }
+      
         p{
             font-size: 14px;
             opacity: .7;
@@ -203,13 +328,23 @@
         }
     }
     .cart-body{
-        img{
-            width: 80px;
-            height: 80px;
+        .inner{
             display: flex;
-            justify-content: center;
-            text-align: center;
-            align-items: center;
+            div{
+                padding:5px;
+                &:first-child{
+                    flex:1;
+                }
+                &:last-child{
+                    flex:4;
+                    min-width: 230px;
+                    padding: 0 11px;
+                }
+            }
+        }
+        img{
+             min-width: 88px;
+             margin-bottom: 13px;
         }
         select{
             font-size: 10px;
@@ -218,12 +353,25 @@
         a{
             color: rgba(49, 49, 49, 0.71);
             font-weight: 300;
+           @media(max-width: 425px){
+               font-size: 17px;
+           }
             &:hover{
                 text-decoration: underline;
             }
         }
     }
     .cart-header{
+        @media(max-width: 768px){
+            box-shadow: 1px 1px 14px #00000014;
+            position: fixed;
+            top: -2px;
+            background: #fff;
+            right: -2px;
+            left: -2px;
+            padding: 12px 17px;
+            z-index: 10;
+        }
 
         .ow-dropdown{
             position: relative;
@@ -284,7 +432,7 @@
         .col-md-3{
             max-width: 100%;
         }
-
+    
     }
     @media (max-width: 1024px) {
 
