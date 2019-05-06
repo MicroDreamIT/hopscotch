107a1bf80eaeece3b652b81b22264e56b3009432
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2019-05-06 18:13:12 +0600

    commit on product page

---
+++ pages/checkout/checkout-page.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		 <div class="container">
+			<div class="row">
+				<div class="col-lg-2"></div>
+				<div class="col-lg-8">
+					<div class="row">
+						<div class="col-md-9">
+							<br>
+							<h2>Checkout</h2>
+							<hr>
+							<div class="steps">
+								<div class="tab">Mobile</div>
+								<div class="tab-content">
+									<div class="mobile-number">
+										<h4>Enter your mobile number</h4>
+										<p>Your shipping and payment details will be <br>
+											associated with this number
+										</p>
+										<template>
+											<mdb-input box label="Your mobile number"/>
+											<mdb-btn color="pink" class="" block > CONTINUE</mdb-btn>
+										</template>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">sad</div>
+					</div>
+				</div>
+				<div class="col-lg-2"></div>
+			</div>
+		 </div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
